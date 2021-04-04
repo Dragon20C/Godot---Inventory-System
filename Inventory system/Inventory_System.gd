82869cbd1e3dc9845df0b4794dc212896d9db7db
Dragon20C <dragon212c@gmail.com>
2021-04-04 08:37:31 +0100

    A mess

--- Inventory system/Inventory_System.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-var slot_class = preload("res://slots.gd")
-onready var inv_slots = $foreground1/GridContainer
-var holding_item = null
-
-func _ready():
-	for inv_slot in inv_slots.get_children():
-		inv_slot.connect("gui_input",self,"slot_gui_input",[inv_slot])
